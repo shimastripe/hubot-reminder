9446f660edee0da73b30d20ad916882c220ad06c
Go Takagi <takagi@shimastripe.com>
2017-10-18 15:17:36 +0900

    Format

---
+++ src/reminder.js
@@ -0,0 +1,16 @@
+'use strict';
+// Description
+//   Schedule reminder for lab slack
+//
+// Configuration:
+//   
+//
+// Commands:
+//   /reminder-toggle <boolean>
+//
+// Author:
+//   Go Takagi <takagi@shimastripe.com>
+
+module.exports = robot => {
+
+};
